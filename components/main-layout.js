5526dbf7126a9ed600c8cacf0a91ff54d08462dd
Norman <bcaaab@gmail.com>
2025-07-20 13:15:46 +0200

    temp

--- components/main-layout.js
+++ components/main-layout.js
@@ -75,7 +75,7 @@ main {
 footer {
   background: #21262d;
   padding: 1rem;
-  margin: auto -1.5rem 0 -1.5rem;
+  margin: auto -1.5rem -1.5rem -1.5rem;
   border-top: 1px solid #30363d;
   margin-top: auto;
 }
